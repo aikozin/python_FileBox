a8b35fd58948484966ee3e5deca8a6f90d066b30
aikozin <47672928+aikozin@users.noreply.github.com>
2023-03-22 11:22:25 +0300

    Доработка функции сборщика мусора (#5)
    Co-authored-by: Greg <skycrew92@gmail.com>

--- utils/trash_collector.py
+++ utils/trash_collector.py
@@ -2,6 +2,7 @@ import os
 import db_connector
 import threading
 from datetime import datetime
+import main
 
 
 class TrashCollector(threading.Thread):
@@ -15,36 +16,38 @@ class TrashCollector(threading.Thread):
 
 
 def trash_proc():
-    print(f'\ntrash_collector in progress...{datetime.now()}')  # {datetime.now():%y-%m-%d %H:%M:%S}
+    # print(f'\ntrash_collector in progress...{datetime.now()}')  # {datetime.now():%y-%m-%d %H:%M:%S}
+    deletes_data = set()  # контейнер для хранения имён файлов вида {'file_name_fs.txt', ...}
     db = db_connector.create_connection()
     query = 'SELECT id_session FROM session WHERE now() > time_end'
     with db.cursor() as cursor:
         cursor.execute(query)
         deletes_sessions = tuple((id_session for id_session in cursor.fetchall()))
 
-    data_query = 'SELECT file_name_fs FROM data WHERE id_session = %s'
-    deletes_data = set()
-    for value in deletes_sessions:
-        with db.cursor() as cursor:
-            cursor.execute(data_query, value)
-            # deletes_data.add(*cursor.fetchall())
-            [deletes_data.add(name) for name in cursor.fetchall()]
     data_query = 'SELECT file_name_fs FROM data WHERE now() > time_death'
     with db.cursor() as cursor:
         cursor.execute(data_query)
-        [deletes_data.add(name) for name in cursor.fetchall()]
+        [deletes_data.add(*file_name_fs) for file_name_fs in cursor.fetchall()]
 
-    session_query = 'DELETE FROM session WHERE id_session = %s'
-    for value in deletes_sessions:
-        with db.cursor() as cursor:
-            cursor.execute(session_query, value)
-            db.commit()
-    db.close()
-    [os.remove(*file_path) for file_path in deletes_data if os.path.exists(*file_path)]
-    return print(f'\nSessions deleted: {deletes_sessions}{datetime.now()}' if deletes_sessions
-                 else f'\nNothing was deleted...{datetime.now()}')
+    if deletes_sessions:
+        data_query = 'SELECT file_name_fs FROM data WHERE id_session = %s'
+        for value in deletes_sessions:
+            with db.cursor() as cursor:
+                cursor.execute(data_query, value)
+                # ответ от БД имеет вид {('file_name_fs.txt',)}
+                # распаковываем до вида {'file_name_fs.txt',}
+                [deletes_data.add(*file_name_fs) for file_name_fs in cursor.fetchall()]
 
+        session_query = 'DELETE FROM session WHERE id_session = %s'
+        for value in deletes_sessions:
+            with db.cursor() as cursor:
+                cursor.execute(session_query, value)
+                db.commit()
+        db.close()
 
-# def start():
-#     trash_collector = TrashCollector()
-#     trash_collector.start()
+    if deletes_data:
+        [os.remove(os.path.join(main.UPLOAD_FOLDER, file_name_fs)) for file_name_fs in deletes_data
+         if os.path.exists(os.path.join(main.UPLOAD_FOLDER, file_name_fs))]
+    #     print(f'Files deleted: {deletes_data}')
+    # return print(f'\nSessions deleted: {deletes_sessions}{datetime.now()}' if deletes_sessions
+    #              else f'\nNothing was deleted...{datetime.now()}')
