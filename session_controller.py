a674da09079ee6bdff64eadea6d4f143eeb2ad4d
GOTYR6 <120591878+GOTYR6@users.noreply.github.com>
2023-04-02 21:28:15 +0300

    feature/FB-5/session_mobile_connect_modify
    * В метод session_mobile_connect добавлен параметр life_time
    
    * Изменил входных параметры в функции update_time_end
    
    ---------
    
    Co-authored-by: aikozindev <aikozindev@gmail.com>

--- session_controller.py
+++ session_controller.py
@@ -37,7 +37,7 @@ def session_start(id_session, web_ip, web_agent):
 
     db = db_connector.create_connection()
     time_start = datetime.now()
-    time_end = time_start + timedelta(minutes=5)
+    time_end = time_start + timedelta(minutes=config.LIFE_TIME)
     query = 'insert into session (id_session, time_start, time_end, web_ip, web_agent) values (%s, %s, %s, %s, %s)'
     val = (id_session, time_start, time_end, web_ip, web_agent)
     with db.cursor() as cursor:
@@ -56,8 +56,8 @@ def session_mobile_connect(id_session, mobile_ip, mobile_agent):
     :return: -
     """
 
+    query = 'UPDATE session SET mobile_ip=%s, mobile_agent=%s, WHERE id_session=%s'
     db = db_connector.create_connection()
-    query = 'UPDATE session SET mobile_ip = %s, mobile_agent = %s WHERE id_session = %s'
     val = (mobile_ip, mobile_agent, id_session)
     with db.cursor() as cursor:
         cursor.execute(query, val)
@@ -92,18 +92,20 @@ def get_session_info(id_session):
     return row
 
 
-def update_time_end(id_session):
+def update_time_end(id_session, infinity=False):
     """
     Метод для обновления времени закрытия сессии
 
     :param id_session: ID сессии
+    :param infinity: параметр для установки бесконечной времени жизни сессии, значение по умолчанию - False
     :return: -
     """
-
-    time_current = datetime.now()
-    time_end = time_current + timedelta(minutes=5)
-    db = db_connector.create_connection()
+    current_time = datetime.now()
+    time_end = current_time + timedelta(minutes=config.LIFE_TIME)
+    if infinity:
+        time_end = current_time + timedelta(days=config.INFINITY_LIFE_TIME)
     query = 'UPDATE session SET time_end = %s WHERE id_session = %s'
+    db = db_connector.create_connection()
     val = (time_end, id_session)
     with db.cursor() as cursor:
         cursor.execute(query, val)
